4265b6a4911fdfd03466832056b119a5c781bd68
Kovalsky <152983058+Kovalssky@users.noreply.github.com>
2025-06-13 13:30:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='kobalt',
         version='0.1.0',
             packages=find_packages(),
-                install_requires=[
-                        "requests"
-                            ],
                                 description='Работа с Cobalt API',
                                     long_description=open('README.md').read(),
                                         long_description_content_type='text/markdown',
